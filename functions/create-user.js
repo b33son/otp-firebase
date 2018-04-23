cb630c54006347ed1775652961335738a023477e
b33son <b33son@gmail.com>
2018-04-22 21:43:39 -0700

    user auth fix

--- functions/create-user.js
+++ functions/create-user.js
@@ -1,25 +1,20 @@
-/*
- * File: /Users/michaelbeeson/Documents/VSCode/react-native-ud/one-time-password/functions/create-user.js
- */
-
 const admin = require("firebase-admin");
 
-// req - request, res - reponse
 module.exports = function(req, res) {
-  // Verify the user provided a phone number
+  // Verify the user provided a phone
   if (!req.body.phone) {
-    return res.status(422).send({ error: "Bad input" });
+    return res.status(422).send({ error: "Bad Input" });
   }
 
-  // Format phone number to remove dashes, spaces and parens
-  const phone = String(req.body.phone).replace(/[^\d]/g, ""); // /[^\d]/g match globally any non digits
+  // Format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
-  // Create a new user account using the phone number
+  // Create a new user account using that phone number
   admin
     .auth()
-    .createUser({ uid: phone }) // async request
+    .createUser({ uid: phone })
     .then(user => res.send(user))
     .catch(err => res.status(422).send({ error: err }));
 
-  // Respone to the user request, saying the account was made
+  // Respond to the user request, saying the account was made
 };
