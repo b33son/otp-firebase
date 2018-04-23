f46fbef54f542061cb5490ade44f4ecf2d56a3fb
b33son <b33son@gmail.com>
2018-04-23 15:23:37 -0700

    reorg

--- functions/twilio.js
+++ functions/twilio.js
@@ -3,8 +3,8 @@
  */
 
 const twilio = require("twilio");
+const twilioAccount = require("./account/twilio-account");
+// const accountSid = "ACa56e0169e5544762e492afedde96e965";
+// const authToken = "3181a35d47a1a96e9cd366f6df212122";
 
-const accountSid = "ACa56e0169e5544762e492afedde96e965";
-const authToken = "3181a35d47a1a96e9cd366f6df212122";
-
-module.exports = new twilio(accountSid, authToken);
+module.exports = new twilio(twilioAccount.accountSid, twilioAccount.authToken);
